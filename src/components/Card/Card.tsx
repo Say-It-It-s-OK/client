95160e4b86438ca54f93cc80e5f15b049d366be3
choi_hyk <blindlchoil@gmail.com>
2025-04-07 22:07:39 +0900

    [CLNT] #7: 장바구니 카드, 옵션 화면 구현

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -35,4 +35,74 @@ const MenuCard = () => {
     );
 };
 
-export { MenuCard };
+const DivOptionCardContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 50%;
+    height: 70%;
+    margin-top: 2%;
+`;
+
+const DivOptionCard = styled.div`
+    width: 73%;
+    aspect-ratio: 1 / 1;
+    background-color: #ffffff;
+    border: none;
+    border-radius: 15px;
+    margin-bottom: 5%;
+`;
+
+const DivOptionCardName = styled.div`
+    width: 85%;
+    height: 12%;
+    background-color: var(--primary-color);
+    border-radius: 15px;
+    border: 4px solid var(--border-color);
+`;
+
+const OptionCard = () => {
+    return (
+        <DivOptionCardContainer>
+            <DivOptionCard />
+            <DivOptionCardName />
+        </DivOptionCardContainer>
+    );
+};
+
+const DivCartCardContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 25%;
+    height: 55%;
+    margin-top: 2%;
+`;
+
+const DivCartCard = styled.div`
+    width: 73%;
+    aspect-ratio: 1 / 1;
+    background-color: #ffffff;
+    border: none;
+    border-radius: 15px;
+    margin-bottom: 5%;
+`;
+
+const DivCartCardName = styled.div`
+    width: 85%;
+    height: 17%;
+    background-color: var(--primary-color);
+    border-radius: 15px;
+    border: 4px solid var(--border-color);
+`;
+
+const CartCard = () => {
+    return (
+        <DivCartCardContainer>
+            <DivCartCard />
+            <DivCartCardName />
+        </DivCartCardContainer>
+    );
+};
+
+export { MenuCard, OptionCard, CartCard };
---
+++ src/components/Menu/Option/optionContainer.tsx
@@ -0,0 +1,66 @@
+import styled from "styled-components";
+import {
+    PaymentButton,
+    AddCartButton,
+    OptionButtonDouble,
+    OptionButtonTriple,
+} from "../../Buttons/buttons";
+import { OptionCard } from "../../Card/card";
+
+const DivOptionContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: flex-center;
+    width: 100%;
+    height: 100%;
+    position: relative;
+    padding: 2%;
+`;
+
+const DivOptionTitle = styled.div`
+    display: flex;
+    width: 40%;
+    height: 8%;
+    background-color: var(--secondary-color);
+    color: white;
+    font-family: var(--font-main);
+    font-size: 200%;
+    justify-content: center;
+    align-items: center;
+    border: none;
+    border-radius: 15px;
+    position: absolute;
+    left: 78%;
+    transform: translateX(-50%);
+`;
+
+const DivOptionButtonContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 40%;
+    height: 60%;
+    align-items: center;
+    position: absolute;
+    top: 15%;
+    left: 78%;
+    transform: translateX(-50%);
+    gap: 4%;
+`;
+
+const OptionContainer = () => {
+    return (
+        <DivOptionContainer>
+            <DivOptionTitle>추가 옵션</DivOptionTitle>
+            <OptionCard />
+            <DivOptionButtonContainer>
+                <OptionButtonDouble />
+                <OptionButtonTriple />
+                <OptionButtonTriple />
+            </DivOptionButtonContainer>
+            <PaymentButton />
+            <AddCartButton />
+        </DivOptionContainer>
+    );
+};
+
+export default OptionContainer;
